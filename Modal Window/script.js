706692ea9887a929463af3cfb7479f93db8a1d41
Akhilesh Gowda <50296948+akhileshgowda7@users.noreply.github.com>
2021-03-29 11:32:31 +0530

    Modal Window
    Modal Window project. where the modal window appears when clicked on the button and disappears when either escape key is pressed or a close button is clicked

---
+++ Modal Window/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const modal =document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+
+const OpenModal = function(){
+    console.log('Button Clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    
+}
+for(let i=0;i<btnsOpenModal.length;i++)
+    btnsOpenModal[i].addEventListener('click',OpenModal);
+
+    const CloseModal=function(){
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+
+    btnCloseModal.addEventListener('click',CloseModal);
+    overlay.addEventListener('click',CloseModal);
+
+    document.addEventListener('keydown',function(e){
+        if(e.key === 'Escape'){
+            if(!modal.classList.contains('hidden')){
+                CloseModal();
+            }
+        }
+
+    })
