c865a85db3465003625f8f06a26c9b29dcbf7d08
Akhilesh Gowda <50296948+akhileshgowda7@users.noreply.github.com>
2021-04-13 13:14:56 +0530

    Short Circuiting
    Short Circuiting in JavaScript.

---
+++ ShortCircuit.js
@@ -0,0 +1,16 @@
+var a;
+var b = null;
+var c = undefined;
+var d = 4;
+var e = 'five';
+
+var f = a || b || c || d || e;
+
+console.log(f);
+
+/* var a; // undefined (falsy value)
+var b = null; // null (falsy value)
+var c = undefined; undefined (falsy value)
+var d = 4; // number (NOT falsy)
+var e = 'five'; // assigment short circuits before reaching here
+*/
