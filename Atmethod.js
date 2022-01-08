9f3e0e4b37f888467b825628eb24d5582c8298f7
akhilesh <akhileshgowda7@gmail.com>
2022-01-08 13:23:55 -0500

    usingg the At method

---
+++ Atmethod.js
@@ -0,0 +1,9 @@
+arr = [1,56,67];
+
+console.log(arr[1]);
+console.log(arr.at(1));
+
+//getting last array element
+console.log(arr[arr.length-1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
