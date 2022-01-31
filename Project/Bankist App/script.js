cb3e84d3838a8f3a82e543352e9cf4d2061af43f
akhilesh <akhileshgowda7@gmail.com>
2022-01-31 16:35:00 -0500

    implimenting loan transfer feature.

--- Project/Bankist App/script.js
+++ Project/Bankist App/script.js
@@ -217,3 +217,18 @@ btnClose.addEventListener('click',function(e){
   }
   inputTransferAmount.value = inputTransferTo.value = '';
 })
+
+btnLoan.addEventListener('click',function(e){
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount>0 && currentAccount.movements.some(mov=>mov>=amount*0.1)){
+    //add movement
+    currentAccount.movements.push(amount);
+
+    //updating user interface
+
+    updateUI(currentAccount)
+  }
+  inputLoanAmount.value='';
+})
