d3d75a9cfd402f43966cb556a763544ea8785796
akhilesh <akhileshgowda7@gmail.com>
2022-02-07 21:34:01 -0500

    added the sorting feature

--- Project/Bankist App/script.js
+++ Project/Bankist App/script.js
@@ -74,9 +74,12 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-const displayMovements = function(movements){
+const displayMovements = function(movements, sort=false){
   containerMovements.innerHTML='';
-  movements.forEach(function(mov,i){
+
+  const movs= sort ? movements.slice().sort((a,b)=> a-b):movements;
+
+  movs.forEach(function(mov,i){
     const type = mov>0 ? 'deposit' : 'withdrawal';
 
     const html = `
@@ -232,3 +235,11 @@ btnLoan.addEventListener('click',function(e){
   }
   inputLoanAmount.value='';
 })
+
+let sorted=false;
+
+btnSort.addEventListener('click',function(e){
+  e.preventDefault();
+  displayMovements(currentAccount.movements,!sorted);
+  sorted=!sorted;
+});
