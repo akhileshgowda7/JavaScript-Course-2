960a8347d872466ceeee4d690c129aca62c1e7eb
akhilesh <akhileshgowda7@gmail.com>
2022-03-08 15:01:10 -0500

    Numeric separators js

---
+++ practice methods/numericSeparators.js
@@ -0,0 +1,11 @@
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee= 45_00;
+console.log(transferFee);
+
+const PI = 3.1415
+console.log(PI);
