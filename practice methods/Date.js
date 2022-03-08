a90672e9d783d4aca2399b310cc63a5284880aca
akhilesh <akhileshgowda7@gmail.com>
2022-03-08 15:29:29 -0500

    Date object in js

---
+++ practice methods/Date.js
@@ -0,0 +1,12 @@
+const now = new Date()
+console.log(now);
+
+const future = new Date(2037,10,19,15,23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.toISOString);
